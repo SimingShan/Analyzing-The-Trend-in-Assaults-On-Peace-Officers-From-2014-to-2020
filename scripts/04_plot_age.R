1c5e6de6cce3dfb59400af9ff8c987b78f9e6ae1
SimingShan <shansiming@gmail.com>
2022-02-07 00:09:32 +0000

    Save before take a break

--- scripts/04_plot_age.R
+++ scripts/04_plot_age.R
@@ -9,7 +9,15 @@
 ### and their corresponding total number of cases
 age_assault <- data_cleaned %>% group_by(age_cohort) %>% summarise(age_count=sum(count))
 
+age_assault <- age_assault %>% filter(age_cohort != "Unknown")
 
+sum_age <- sum(age_assault$age_count)
+
+age_assault <- age_assault %>% mutate(police_age_percentage = c(0.018,2.31,28.66,37.53,27.07,3.63,0.75))
+
+age_assault <- age_assault %>% mutate(victim_age_percentage = age_count/sum_age * 100) 
+
+age_assault <- age_assault %>% mutate(net_percentage = police_age_percentage - victim_age_percentage)
 ### create the plot
 age_assault %>% 
   ggplot(aes(x=age_cohort,y=age_count)) +
@@ -19,3 +27,7 @@ age_assault %>%
        y="Count of Assaults From 2014 - 2020")+
   ggtitle("Age cohort of identified victim vs Count of identified victims")+
   geom_text(aes(label=age_count),vjust=-0.3)
+
+### create the table
+
+knitr::kable(age_assault,format = "pipe",digits = 2)
